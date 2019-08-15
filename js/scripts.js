68bc70df912366655ae92de314c4b3ce44ee6ea7
AndyOmondi17 <andyomondi17@gmail.com>
2019-08-15 10:27:41 +0300

    multiplication functions

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*var add = function(number1, number2) {
+return number1 + number2;
+};
+var number1=parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter the second number"));
+var result = add(number1,number2);
+alert("Your sum is "+ result);
+
+function subtract(number1,number2){
+  var number1=parseInt(prompt("Enter a number"));
+  var number2=parseInt(prompt("Enter the second number"));
+  return number1 - number2;
+
+};
+alert("Your sum is " + subtract());*/
+
+function multiply(number1,number2){
+  var number1=parseInt(prompt("Enter a number"));
+  var number2=parseInt(prompt("Enter the second number"));
+  return number1 * number2;
+
+};
+alert("Your sum is " + multiply());
